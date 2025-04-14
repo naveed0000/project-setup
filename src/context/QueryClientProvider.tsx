640f943acdec176c71a5c364d1cc87ffa1ef8efd
naveed0000 <sknaveed99026@gmail.com>
2025-04-14 07:38:19 +0530

    just added props

--- src/context/QueryClientProvider.tsx
+++ src/context/QueryClientProvider.tsx
@@ -8,7 +8,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ReactNode, useState } from 'react';
 
 interface QueryClientProviderProps {
-  children: ReactNode;
+  children: ReactNode;    
 }
 
 export default function QueryClientProvider({
